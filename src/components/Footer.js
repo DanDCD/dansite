5653f3a09f6aa530f0279351373cb7eb4aa3f3da
Daniel Drew <daniel.drew2014@outlook.com>
2024-07-29 17:38:18 +0100

    feat: improve header scaling for small devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import "../index.css";
 const StyledFooter = Styled.footer`
     /* grid element settings */
     grid-area: footer;
-    background-color: #462255;
+    background-color: #313B72;
     padding: var(--header-footer-spacing);
     `;
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,7 @@ const StyledHeader = Styled.header`
     /* flex container settings */
     display: flex;
     justify-content: space-between;
+    flex-wrap: nowrap;
 `;
 
 const StyledNav = Styled.nav`
@@ -20,15 +21,42 @@ const StyledNav = Styled.nav`
     align-items: center;
 `;
 
+const StyledH1 = Styled.h1`
+    color: white;
+    whitespace: nowrap;
+    /* flex item settings */
+    flex-shrink: 1;
+    font-size: calc(1vw + 1rem); /* Dynamically adjust font size based on viewport width */
+`;
+
 const StyledButton = Styled.button`
     padding: 5px 10px;
     margin: 5px;
+    /* flex item settings */
+    flex-shrink: 1;
+    font-size: calc(1vw + 0.5rem); /* Dynamically adjust font size based on viewport width */
+
+    /* Default styles for mobile */
+    background-color: #F5FFFA;
+    color: black;
+    border: none;
+    border-radius: 10%;
+    
+
+    /* Styles for desktop */
+    @media (min-width: 768px) {
+        background-color: transparent;
+        color: #F5FFFA;
+        border: none;
+    }
 `;
+
+
 
 const Header = ({ section, setSection }) => {
     return (
         <StyledHeader>
-            <h1>Daniel Drew</h1>
+            <StyledH1>Daniel Drew</StyledH1>
             <StyledNav>
                 <StyledButton onClick={() => setSection('home')}>Home</StyledButton>
                 <StyledButton onClick={() => setSection('about')}>About</StyledButton>
